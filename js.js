7dcd6454d169bdca44312876496dfd5f9e85cfc7
Igor-Lysak <lisakigor485@gmail.com>
2023-01-14 22:09:29 +0200

    second push

--- js.js
+++ js.js
@@ -1,1 +1,16 @@
+let pag_link = document.querySelector(".pag-link");
 
+pag_link.addEventListener("click", function(){
+    this.pag_link.classList.add("active")
+    console.log("great!")
+});
+
+
+let mask = document.querySelector(".mask");
+
+window.addEventListener("load", () => {
+    mask.classList.add("hide");
+    setTimeout(() => {
+        mask.remove();
+    },600);
+});
